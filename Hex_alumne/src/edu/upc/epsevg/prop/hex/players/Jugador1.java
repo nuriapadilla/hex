da5b48505552ba784f40281da2b82c7e99668d5f
nuriapadilla <nuria.padilla@estudiantat.upc.edu>
2024-11-26 11:17:38 +0100

    fitxers profe

---
+++ Hex_alumne/src/edu/upc/epsevg/prop/hex/players/Jugador1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.upc.epsevg.prop.hex.players;
+
+import edu.upc.epsevg.prop.hex.HexGameStatus;
+import edu.upc.epsevg.prop.hex.IAuto;
+import edu.upc.epsevg.prop.hex.IPlayer;
+import edu.upc.epsevg.prop.hex.PlayerMove;
+
+/**
+ *
+ * @author nuria
+ */
+public class Jugador1 implements IPlayer, IAuto{
+
+    @Override
+    public PlayerMove move(HexGameStatus hgs) {
+    }
+
+    @Override
+    public void timeout() {
+        //S'ha de canviar un boolea
+        
+    }
+
+    @Override
+    public String getName() {
+        
+    }
+    
+}
