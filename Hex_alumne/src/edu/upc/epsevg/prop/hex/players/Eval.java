ad5a8cb05e5a26ef5ffab05a361451ab9f5c8e5b
nuriapadilla <nuria.padilla@estudiantat.upc.edu>
2024-12-30 23:17:51 +0100

    Eval-javadoc

--- Hex_alumne/src/edu/upc/epsevg/prop/hex/players/Eval.java
+++ Hex_alumne/src/edu/upc/epsevg/prop/hex/players/Eval.java
@@ -7,13 +7,23 @@ package edu.upc.epsevg.prop.hex.players;
 import java.awt.Point;
 
 /**
- *
+ * La classe {@code Eval} representa un moviment avaluat en el joc Hex.
+ * Conté un punt del tauler i el valor heurístic associat, utilitzat per a 
+ * prioritzar jugades durant l'exploració de moviments.
+ * 
  * @author nuria
  */
 public class Eval {
     Point punt;
     double heur;
 
+    /**
+     * Constructor per inicialitzar un objecte {@code Eval} amb un punt i el seu
+     * valor heurístic.
+     *
+     * @param p el punt del tauler associat al moviment.
+     * @param h el valor heurístic assignat al moviment.
+     */
     public Eval(Point p, double h) {
         punt = p;
         heur = h;
